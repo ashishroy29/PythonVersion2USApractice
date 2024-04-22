602e05cc099012c2433d3032b51aa3cdac236445
Ashish Roy <84368577+ashishroy29@users.noreply.github.com>
2024-04-22 12:25:46 -0400

    Add files via upload

---
+++ Pythonpractice.py
@@ -0,0 +1,84 @@
+print("Hello my name is Ashish")
+
+print(len("Hello my name is ashish"))
+
+1+2
+
+3/7
+
+a = 1234
+print(a)
+
+Roy = 2345
+print(Roy)
+
+print(type(a))
+
+print(type(Roy))
+
+b = 45.12
+print(b)
+print(type(b))
+
+c = "Ashish"
+type(c)
+
+#Complex Number
+d = 5+6j
+print(d)
+print(type(d))
+
+e = "Ash"
+print(e)
+type(e)
+
+
+g = True
+print(g)
+print(type(g))
+
+_a=45
+print(_a)
+
+a=12
+print(a)
+
+a1=2323
+print(a1)
+
+print(d.imag)
+print(d.real)
+
+a,b,c,d,e = 1234,"Ashi",6+7j,True,564.34
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(a+a1)
+
+print(c.real)
+print(c.imag)
+
+print(b.count)
+
+a= 'ashi'
+print(a + "4")
+
+a + str(4)
+
+True and True
+
+True + True
+
+True - False
+
+1 + True
+
+a = input()
+
+a = int(input())
+
+print(a)
+
